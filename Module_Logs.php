219a372600d53c7f27169a3caf986210adf5f418
gizmore <gizmore@wechall.net>
2017-06-25 19:30:01 +0200

    sync

--- Module_Logs.php
+++ Module_Logs.php
@@ -4,7 +4,15 @@ final class Module_Logs extends GWF_Module
 	public function getConfig()
 	{
 		return array(
-			GDO_Checkbox::make('log_requests')->initial('0'),	
+			GDO_Checkbox::make('log_requests')->initial('0'),
+			GDO_Checkbox::make('log_rotation_mail')->initial('1'),
+			GDO_Checkbox::make('log_rotation')->initial('1'),
+			GDO_Date::make('last_log_rotation'),
 		);
 	}
+	
+	public function cfgLogRequests() { return $this->getConfigValue('log_requests'); }
+	public function cfgLogRotationMail() { return $this->getConfigValue('log_rotation_mail'); }
+	public function cfgLogRotation() { return $this->getConfigValue('log_rotation'); }
+	public function cfgLastRotation() { return $this->getConfigValue('last_log_rotation'); }
 }
---
+++ method/Rotate.php
@@ -0,0 +1,27 @@
+<?php
+final class Logs_Rotate extends GWF_MethodCronjob
+{
+	public function run()
+	{
+		$module = Module_Logs::instance();
+		
+		if ($module->cfgLogRotation())
+		{
+			$now = date('Ymd');
+			$last = $module->cfgLastRotation();
+			$now = GWF_Time::getDate(mktime(0,0,0));
+			if ($last != $now)
+			{
+// 				var_dump($last, $now);
+				$this->logRotate();
+// 				$module->saveConfigValue('log_last_rotation', $now);
+			}
+		}
+	}
+	
+	public function logRotate()
+	{
+		$this->logNotice('Log Rotation');
+		
+	}
+}
